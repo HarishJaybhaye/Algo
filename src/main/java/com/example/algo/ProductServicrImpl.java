bf39c958322038a28a6a0cc633c5d15ff61995fd
HarishJaybhaye <39984760+HarishJaybhaye@users.noreply.github.com>
2022-09-30 11:16:16 +0530

    service changes

--- src/main/java/com/example/algo/ProductServicrImpl.java
+++ src/main/java/com/example/algo/ProductServicrImpl.java
@@ -11,61 +11,46 @@ public class ProductServicrImpl implements ProductServic {
 	private ProductRepository productRepository;
 
 	public void addProduct(Product product) throws Exception {
-		 product = getProductById(product.getProductId());
-		Double discount = 0.0;
-		Double gst = 0.0;
-		Double deliveyCharge = 0.0;
-		Double charges = 0.0;
-		product.setProductCategory(product.getProductCategory());
-		product.setProductName(product.getProductName());
-		product.setProductType(product.getProductType());
-		product.setBasePrice(product.getBasePrice());
-		if (product.getProductCategory().equalsIgnoreCase("ELECTRONICS")) {
-			Double gstE = 18.0;
-			discount = 15.0;
-			Double finalAmount = 0.0;
-			gst = product.getBasePrice().doubleValue() * gstE / 100;
-			deliveyCharge = 350.0;
-			product.setGst(gst.longValue());
-			product.setDiscount(product.getBasePrice().doubleValue() + gst);
-			product.setDeliveyCharge(Double.valueOf(deliveyCharge));
-			finalAmount = gst + deliveyCharge + product.getBasePrice().doubleValue();
-			product.setFinalAmount(finalAmount.doubleValue());
-		} else if (product.getProductCategory().equalsIgnoreCase("Home Appliances")) {
-			Double gstE = 24.0;
-			discount = 22.0;
-			Double finalAmount = 0.0;
-			gst = product.getBasePrice().doubleValue() * gstE / 100;
-			deliveyCharge = 800.0;
-			product.setGst(gst.longValue());
-			product.setDiscount(product.getBasePrice().doubleValue() + gst);
-			product.setDeliveyCharge(Double.valueOf(deliveyCharge));
-			finalAmount = gst + deliveyCharge + product.getBasePrice().doubleValue();
-			product.setFinalAmount(finalAmount.doubleValue());
-		} else if (product.getProductCategory().equalsIgnoreCase("Clothing")) {
-			Double gstE = 12.0;
-			discount = 40.0;
-			Double finalAmount = 0.0;
-			gst = product.getBasePrice().doubleValue() * gstE / 100;
-			deliveyCharge = 0.0;
-			product.setGst(gst.longValue());
-			product.setDiscount(product.getBasePrice().doubleValue() + gst);
-			product.setDeliveyCharge(Double.valueOf(deliveyCharge));
-			finalAmount = gst + deliveyCharge + product.getBasePrice().doubleValue();
-			product.setFinalAmount(finalAmount.doubleValue());
-		}else if (product.getProductCategory().equalsIgnoreCase("Furniture")) {
-			Double gstE = 18.0;
-			discount = 10.0;
-			Double finalAmount = 0.0;
-			gst = product.getBasePrice().doubleValue() * gstE / 100;
-			deliveyCharge = 300.0;
-			product.setGst(gst.longValue());
-			product.setDiscount(product.getBasePrice().doubleValue() + gst);
-			product.setDeliveyCharge(Double.valueOf(deliveyCharge));
-			finalAmount = gst + deliveyCharge + product.getBasePrice().doubleValue();
-			product.setFinalAmount(finalAmount.doubleValue());
-		}
-
+		/*
+		 * product = getProductById(product.getProductId()); Double discount = 0.0;
+		 * Double gst = 0.0; Double deliveyCharge = 0.0; Double charges = 0.0;
+		 * product.setProductCategory(product.getProductCategory());
+		 * product.setProductName(product.getProductName());
+		 * product.setProductType(product.getProductType());
+		 * product.setBasePrice(product.getBasePrice()); if
+		 * (product.getProductCategory().equalsIgnoreCase("ELECTRONICS")) { Double gstE
+		 * = 18.0; discount = 15.0; Double finalAmount = 0.0; gst =
+		 * product.getBasePrice().doubleValue() * gstE / 100; deliveyCharge = 350.0;
+		 * product.setGst(gst.longValue());
+		 * product.setDiscount(product.getBasePrice().doubleValue() + gst);
+		 * product.setDeliveyCharge(Double.valueOf(deliveyCharge)); finalAmount = gst +
+		 * deliveyCharge + product.getBasePrice().doubleValue();
+		 * product.setFinalAmount(finalAmount.doubleValue()); } else if
+		 * (product.getProductCategory().equalsIgnoreCase("Home Appliances")) { Double
+		 * gstE = 24.0; discount = 22.0; Double finalAmount = 0.0; gst =
+		 * product.getBasePrice().doubleValue() * gstE / 100; deliveyCharge = 800.0;
+		 * product.setGst(gst.longValue());
+		 * product.setDiscount(product.getBasePrice().doubleValue() + gst);
+		 * product.setDeliveyCharge(Double.valueOf(deliveyCharge)); finalAmount = gst +
+		 * deliveyCharge + product.getBasePrice().doubleValue();
+		 * product.setFinalAmount(finalAmount.doubleValue()); } else if
+		 * (product.getProductCategory().equalsIgnoreCase("Clothing")) { Double gstE =
+		 * 12.0; discount = 40.0; Double finalAmount = 0.0; gst =
+		 * product.getBasePrice().doubleValue() * gstE / 100; deliveyCharge = 0.0;
+		 * product.setGst(gst.longValue());
+		 * product.setDiscount(product.getBasePrice().doubleValue() + gst);
+		 * product.setDeliveyCharge(Double.valueOf(deliveyCharge)); finalAmount = gst +
+		 * deliveyCharge + product.getBasePrice().doubleValue();
+		 * product.setFinalAmount(finalAmount.doubleValue()); }else if
+		 * (product.getProductCategory().equalsIgnoreCase("Furniture")) { Double gstE =
+		 * 18.0; discount = 10.0; Double finalAmount = 0.0; gst =
+		 * product.getBasePrice().doubleValue() * gstE / 100; deliveyCharge = 300.0;
+		 * product.setGst(gst.longValue());
+		 * product.setDiscount(product.getBasePrice().doubleValue() + gst);
+		 * product.setDeliveyCharge(Double.valueOf(deliveyCharge)); finalAmount = gst +
+		 * deliveyCharge + product.getBasePrice().doubleValue();
+		 * product.setFinalAmount(finalAmount.doubleValue()); }
+		 */
 		productRepository.save(product);
 	}
 
